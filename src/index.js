d27cabd3e9e8073847fb87318b891ef133d0fe5b
Lucas Zerino <101360532+LucasZerino@users.noreply.github.com>
2022-12-02 11:22:13 -0300

    finalização do site
    Final

--- src/index.js
+++ src/index.js
@@ -6,7 +6,17 @@ import path from 'path'
 import session from 'express-session';
 import bodyParser from 'body-parser';
 import { prisma } from "./services/prisma";
-import { compare } from 'bcrypt';
+import nodemailer from 'nodemailer';
+
+const transport = nodemailer.createTransport({
+    host: "smtp.office365.com",
+    port: 587,
+    secure: false, // true para 465, false para outras
+    auth: {
+        user: 'inarenvio@outlook.com',
+        pass: 'ABcd123987@##'
+    }
+})
 
 
 dotenv.config()
@@ -54,6 +64,41 @@ app.post('/blog', (req, res) => {
    res.render('uniqueblog', {api: process.env.API, postid: Postid});
 })
 
+app.get('/contato', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.render('contato', {api: process.env.API});
+})
+
+app.post('/contato', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    console.log("tentou postar")
+    const email = process.env.EMAIL;
+    const subject = req.body.assuntomail;
+    const text = req.body.con_message;
+    const nome = req.body.nomemail;
+    const emailuser = req.body.email;
+    const phone = req.body.phone;
+    if(email != null && subject != null && text != null){
+        if(email != '' && subject != '' && text != ''){
+            if(email != undefined && subject != undefined && text != undefined){
+                transport.sendMail({
+                    from: 'Inar envio <inarenvio@outlook.com>',
+                    to: email,
+                    subject: subject,
+                    text: 
+                    `Email enviado por: ${nome}, Email: ${emailuser}, Telefone: ${phone}
+                    Mensagem: ${text}`,
+                    }).then(info => {
+                    console.log(info)
+                    res.redirect('/agradecimento')
+                })
+            }}
+}})
+
+app.get('/agradecimento', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.render('agradecimento', {api: process.env.API});
+})
 
 app.post('/login', (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
